c8496e4c97384c7c501d3ae2259354806ae66bd3
Mohammad Zaid <42945239+mzs9540@users.noreply.github.com>
2026-04-17 01:16:17 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const sizes = [];
+        for (const s of strs) {
+            sizes.push(s.length)
+        }
+        console.log(sizes.join(',') + '#' + strs.join(''))
+        return sizes.join(',') + '#' + strs.join('');
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        if (str === '#') {
+            return [];
+        }
+        const strs = [];
+        let startIndex = -1;
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === '#') {
+                startIndex = i + 1;
+                break;
+            }
+        }
+        const sizes = str.slice(0, startIndex - 1).split(',');
+        console.log('sizes', sizes)
+        console.log('startIndex', startIndex)
+        for (const s of sizes) {
+            strs.push(str.slice(startIndex, startIndex + parseInt(s)))
+            startIndex += parseInt(s);
+        }
+
+        console.log(strs)
+
+        return strs;
+    }
+}
