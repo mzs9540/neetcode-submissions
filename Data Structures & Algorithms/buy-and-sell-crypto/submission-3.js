2cab7ae8e35efa7e331b2bf960bde25e17ddf710
Mohammad Zaid <42945239+mzs9540@users.noreply.github.com>
2026-06-22 01:03:45 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0;
+        let r = 1;
+        let res = 0;
+         while (r < prices.length) {
+            const pR = prices[r];
+            const pL = prices[l];
+            if (pL > pR) {
+                l = r
+            } else if (pL < pR) {
+                res = Math.max(res, pR - pL);
+            };
+            r++;
+         }
+
+         return res;
+    }
+}
