cc15a995cd52b251b78f1c308dcb5dcef2cb001b
Mohammad Zaid <42945239+mzs9540@users.noreply.github.com>
2026-06-27 12:41:00 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let maxF = 0,
+            res = 0,
+            l = 0;
+        const count = new Map();
+
+        for (let r = 0; r < s.length; r += 1) {
+            count.set(s[r], (count.get(s[r]) || 0) + 1);
+            maxF = Math.max(maxF, count.get(s[r]));
+
+            while (r - l + 1 - maxF > k) {
+                count.set(s[l], count.get(s[l]) - 1);
+                l++;
+            }
+
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
